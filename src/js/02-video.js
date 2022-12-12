df4e72b37c444d48314e0e64edafb10f978541ab
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2022-12-12 15:58:39 +0200

    121122

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,12 +7,12 @@ const player = new Player(iframe);
 player.on(
   'timeupdate',
   throttle(evt => {
-    localStorage.setItem('videoplayer-current-time', evt.seconds);
+    localStorage.setItem('videoplayerCurrentTime', evt.seconds);
   }, 1000)
 );
 
 player
-  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .setCurrentTime(localStorage.getItem('videoplayerCurrentTime'))
   .catch(function (error) {
     console.error(error);
   });
