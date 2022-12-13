78d4f8b513fb5027ec9e64d106e70635c707d642
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2022-12-13 22:11:52 +0200

    1312222

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,65 +1,58 @@
-
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
 const refs = {
-    formEl: document.querySelector(".feedback-form"),
-    inputEl: document.querySelector(".feedback-form input"),
-    textarea: document.querySelector(".feedback-form textarea"),
-    buttonEl: document.querySelector(".feedback-form button"),
-  };
-  
+  formEl: document.querySelector('.feedback-form'),
+  inputEl: document.querySelector('.feedback-form input'),
+  // textarea: document.querySelector('.feedback-form textarea'),
+  buttonEl: document.querySelector('.feedback-form button'),
+};
+
 const SUBMITTING_FORM = 'feedback-form-state';
 
-refs.formEl.addEventListener("submit", onFormSubmit);
-refs.inputEl.addEventListener("input", throttle(onTextareaInput, 500));
-refs.textarea.addEventListener("input", throttle(onTextarea, 500));
+refs.formEl.addEventListener('submit', onFormSubmit);
+refs.inputEl.addEventListener('input', throttle(onTextareaInput, 500));
+// refs.textarea.addEventListener('input', throttle(onTextarea, 500));
 // refs.buttonEl.addEventListener();
-
+refs.formEl.addEventListener('input', e => {
+  console.log(e.target.name);
+  console.log(e.target.value);
+});
 populateTextarea();
 
-populateInput();
-
-
 function onFormSubmit(evt) {
-    evt.preventDefault();
+  evt.preventDefault();
 
-    console.log('Отправляем форму');
-    evt.currentTarget.reset();
-    localStorage.removeItem(SUBMITTING_FORM);
+  console.log('Отправляем форму');
+  evt.currentTarget.reset();
+  localStorage.removeItem(SUBMITTING_FORM);
 }
 
-
 function onTextareaInput(evt) {
-    console.log(evt.target.value)
-    
-    const message = evt.target.value;
-  
-    localStorage.setItem(SUBMITTING_FORM, message);
+  // console.log(evt.target.value);
+
+  const message = evt.target.value;
+
+  localStorage.setItem(SUBMITTING_FORM, message);
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem(SUBMITTING_FORM);
+
+  if (savedMessage) {
+    refs.inputEl.value = savedMessage;
   }
+}
 
-  function onTextarea(evt) {
-    console.log(evt.target.value)
-    
-    const message = evt.target.value;
-  
-    localStorage.setItem(SUBMITTING_FORM, message);
-  }
+// Домой
+// сделать так чтобы сохраняло не только сообщение но и имя, и все в одном обьекте
 
+// const formData = {};
 
-  function populateTextarea() {
-    const savedMessage = localStorage.getItem(SUBMITTING_FORM);
-  
-    if (savedMessage) {
-      refs.inputEl.value = savedMessage;
-    }
-  }
+// refs.form.addEventListener('input', e => {
+//   // console.log(e.target.name);
+//   // console.log(e.target.value);
 
-  function populateInput() {
-    const savedMessage = localStorage.getItem(SUBMITTING_FORM);
-  
-    if (savedMessage) {
-      refs.inputEl.value = savedMessage;
-    }
-  }
+//   formData[e.target.name] = e.target.value;
 
-
+//   console.log(formData);
+// });
